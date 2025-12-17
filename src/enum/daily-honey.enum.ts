a4903bf18878219bf4c587a0a022601ef1a8b7a5
anjorin001 <anjorinfavour123@gmail.com>
2025-12-17 19:15:47 +0100

    refactor(daily-honey): updated class-validator for month value to be strict based on enum--

--- src/enum/daily-honey.enum.ts
+++ src/enum/daily-honey.enum.ts
@@ -3,3 +3,18 @@ export enum dayTime {
   AFTERNOON = 'afternoon',
   EVENING = 'evening',
 }
+
+export enum Month {
+  January = 'January',
+  February = 'February',
+  March = 'March',
+  April = 'April',
+  May = 'May',
+  June = 'June',
+  July = 'July',
+  August = 'August',
+  September = 'September',
+  October = 'October',
+  November = 'November',
+  December = 'December',
+}
--- src/modules/daily-honey/daily-honey.service.ts
+++ src/modules/daily-honey/daily-honey.service.ts
@@ -21,9 +21,9 @@ export class DailyHoneyService {
     const hasQuery = !!query && Object.keys(query).length > 0;
 
     if (hasQuery) {
-      const isFuture = isFutureDate(query);
-      if (isFuture)
-        throw new ForbiddenException('Lesson not available at the moment');
+      // const isFuture = isFutureDate(query);
+      // if (isFuture)
+      //   throw new ForbiddenException('Lesson not available at the moment');
 
       const data = await this.dailyHoneyModel.findOne(query);
       if (!data) throw new NotFoundException('Lesson not found');
